eff409647caae5dd37782a58eec1bf5b676683fa
bokoboss <188425595+bokoboss@users.noreply.github.com>
2026-09-23 22:12:27 +0700

    Add src/types.ts

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type UseCase = {
+  id: string
+  category: string
+  title: string
+  painPoint: string
+  desiredResult: string
+  users: string
+  level: number
+  surface: string
+  software: string
+  prompt: string
+  guardrail: string
+  moment: string
+  tags: string
+}
